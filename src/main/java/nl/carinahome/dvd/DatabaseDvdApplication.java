80387880294b47008054934d540542d8e3789f38
Carina Horrel <Carina Horrel>
2017-03-24 13:59:00 +0100

    My project defined with spring maven

---
+++ src/main/java/nl/carinahome/dvd/DatabaseDvdApplication.java
@@ -0,0 +1,12 @@
+package nl.carinahome.dvd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseDvdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabaseDvdApplication.class, args);
+	}
+}
